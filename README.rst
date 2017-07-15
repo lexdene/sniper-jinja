df788e022dc3c2d27aaf07584c76419444373e9a
Elephant Liu <lexdene@gmail.com>
2017-07-15 19:01:00 +0800

    create project

---
+++ README.rst
@@ -0,0 +1,4 @@
+sniper-jinja
+============
+
+sniper-jinja is a `jinja2 <http://jinja.pocoo.org/>` plugin for `sniper <http://py-sniper.readthedocs.io/en/master/>`.
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+with open('README.rst') as f:
+    LONG_DESCRIPTION = f.read()
+
+setup(
+    name="sniper_jinja",
+    version="0.0.1",
+    description="a jinja2 plugin for sniper",
+    long_description=LONG_DESCRIPTION,
+    url="https://github.com/lexdene/sniper-jinja",
+    license='GPLv3',
+    author="Elephant Liu",
+    author_email="lexdene@gmail.com",
+    packages=['sniper_jinja'],
+    platforms=['any'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+)
